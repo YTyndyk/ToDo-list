65c6b9f0922b0183d0c3ca82ace7afc01f5f3e6a
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-03-05 14:48:18 +0200

    bugfix todoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const TodoList = ({ items, deleteTodo }) => {
-	const elements = items.map((id, text, completed) => {
+	const elements = items.map(({ id, text, completed }) => (
 		<li key={id}>
-			<p>{text}</p> {completed}
+			{text}.{" "}
 			<button onClick={() => deleteTodo(id)} type="button">
 				Delete
 			</button>
-		</li>;
-	});
+		</li>
+	));
+
 	return <ul>{elements}</ul>;
 };
-
 export default TodoList;
