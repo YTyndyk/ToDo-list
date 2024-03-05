8edcac0e22096f7848b7d40e5c80199b010b7d77
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2024-03-05 08:35:00 +0200

    innitial settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+		"@wemake-services/javascript",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	settings: { react: { version: "18.2" } },
+	plugins: ["react-refresh"],
+	rules: {
+		semi: ["error", "always"],
+		quotes: ["error", "single"],
+		"react/jsx-no-target-blank": "off",
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+	},
+};
